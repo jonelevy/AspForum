1b1c7729f9bc795bae5bad62b644585d5a70630f
jonelevy <jonathan@levysplace.us>
2014-07-25 11:11:49 -0500

    commiting everything

---
+++ WebTest6/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace WebTest6.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int NoCommentsPosted { get; set; }
+        public DateTime? joined { get; set; }
+    }
+    public class UserDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
